2136bedb5ea4a23f87d1784eeb8223ebed7bff8a
Yashpk789987 <43312758+Yashpk789987@users.noreply.github.com>
2019-02-18 03:45:09 +0530

    commited

--- routes/pdf.js
+++ routes/pdf.js
@@ -22,7 +22,7 @@ router.post('/upload_pdf/pg' , upload.single('pdf') , ( req , res ) => {
     let data = JSON.parse(req.body.SendData)
     let query = `insert into pdf_files(sub_category_id , english_name , hindi_name , filename)
     values(${data.sub_category_id},'${data.english_text}','${data.hindi_text}','${req.file.filename}')`
-    pool_1.query(query, (err,result) => {
+    pool_2.query(query, (err,result) => {
         if(err) throw err
         res.json({ message : "Uploaded Successfully ..."})
     })
