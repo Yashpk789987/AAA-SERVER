f1c78d68007025ee70bd9f51fd1ad081d513d755
Yash Kumar <yashpk789987@gmail.com>
2019-07-18 11:40:51 +0530

    commited

--- routes/enquiry.js
+++ routes/enquiry.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var pgsql = require('pg-pool');
 var pool_2 = new pgsql(require('../database').pgsql);
 
-router.get('/submit', (req, res) => {
+router.get('/submit/pg', (req, res) => {
   const { name, mobile_no, address } = req.body;
   let query = `insert into enquiry(name, mobile_no, address) values('${name}','${mobile_no}','${address}')`;
   pool_2.query(query, (err, result) => {
@@ -12,7 +12,7 @@ router.get('/submit', (req, res) => {
   });
 });
 
-router.get('/show_all', (req, res) => {
+router.get('/show_all/pg', (req, res) => {
   let query = 'select * from enquiry';
   pool_2.query(query, (err, result) => {
     if (err) throw err;
