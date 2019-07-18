ddadb64200fc35db16dc79e5835f3e648169b102
Yash Kumar <yashpk789987@gmail.com>
2019-07-18 11:39:02 +0530

    commited

---
+++ routes/enquiry.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var pgsql = require('pg-pool');
+var pool_2 = new pgsql(require('../database').pgsql);
+
+router.get('/submit', (req, res) => {
+  const { name, mobile_no, address } = req.body;
+  let query = `insert into enquiry(name, mobile_no, address) values('${name}','${mobile_no}','${address}')`;
+  pool_2.query(query, (err, result) => {
+    if (err) throw err;
+    res.json({ code: 'enquiry_submit_success' });
+  });
+});
+
+router.get('/show_all', (req, res) => {
+  let query = 'select * from enquiry';
+  pool_2.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result.rows);
+  });
+});
+
+module.exports = router;
